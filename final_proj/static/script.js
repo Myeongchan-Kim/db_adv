2fc3c6d2a8c3e586feda9f746fb4915acbe1fb24
Paul MC Kim <kmc5500@naver.com>
2016-08-24 18:01:58 +0900

    complete transition test

--- final_proj/static/script.js
+++ final_proj/static/script.js
@@ -1,18 +1,33 @@
+var width = 900;
+var height = 600;
 
-var dataset = []
-var xhr = new XMLHttpRequest();
-xhr.open('GET', 'http://localhost:3000/all-country', true);
-xhr.addEventListener("load", function (e){
-  var docs  = JSON.parse(xhr.responseText);
-  init(docs);
-});
-xhr.send(null);
+var init = function (){
+  var dataset = []
+  var xhr = new XMLHttpRequest();
+  xhr.open('GET', 'http://localhost:3000/all-country', true);
+  xhr.addEventListener("load", function (e){
+    var docs  = JSON.parse(xhr.responseText);
+    make_circle(docs);
+  });
+  xhr.send(null);
+}
 
-var init = function (docs){
+var click_event = function (e){
+  if(e.target.tagName == "BUTTON"){
+    changeCircleRandom();
+  }
+}
+
+var changeCircleRandom = function(){
+  var svg = d3.select("svg");
+  var circles = svg.selectAll("circle").transition()
+  .attr("cx", function (d){return Math.random() * width; })
+  .attr("cy", function (d){return Math.random() * height; });
+}
+
+var make_circle = function (docs){
   //console.log(docs);
-  var width = 900;
-  var height = 600;
-  var svg = d3.select("body").append("svg");
+  var svg = d3.select("#screen").append("svg");
   svg.attr("width", width).attr("height", height);
   var circles = svg.selectAll("circle").data(docs).enter().append("circle");
   circles
@@ -25,8 +40,10 @@ var init = function (docs){
 
   var labels = svg.selectAll("text").data(docs).enter().append("text");
   labels.text(function(d){ return d['country_name'];})
-    .attr("x", function(d){return Math.random() * width;})
-    .attr("y", function (d, i){return Math.random() * height;})
-    .attr("color", "gray");
+    .attr("x", function(d){ return Math.random() * width;})
+    .attr("y", function (d, i){ return Math.random() * height;});
     ;
 }
+
+document.addEventListener("click", click_event);
+init();
